67cd920b49896b2db2f34f05da60199d4cccf619
gguilhermelopes <guilhermelopes275@gmail.com>
2023-03-08 14:56:18 -0300

    feat: add button components and add screens

---
+++ components/PrimaryButton.js
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+import { Pressable } from "react-native";
+import { View, Text } from "react-native";
+
+const PrimaryButton = ({ children }) => {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.buttonInnerContainer,
+          pressed && styles.pressed,
+        ]}
+        android_ripple={{ color: "#99024d" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+    elevation: 2,
+  },
+  buttonInnerContainer: {
+    borderRadius: 28,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: "#72063c",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
+
+export default PrimaryButton;
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.input}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginHorizontal: 24,
+    marginTop: 100,
+    borderRadius: 8,
+    backgroundColor: "#4e0329",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  input: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    fontWeight: "bold",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    textAlign: "center",
+  },
+});
+
+export default StartGameScreen;
