8b8e29c3e73f91641e3d8e3a313a026a4037bd8e
gguilhermelopes <guilhermelopes275@gmail.com>
2023-03-08 15:48:50 -0300

    style: add gradient to background start screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 24,
     marginTop: 100,
     borderRadius: 8,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     elevation: 4,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
